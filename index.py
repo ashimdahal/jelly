f12f232ad16299f5a962dc512c11d98c5925c5ee
ashimdahal <bboxchmpx@gmail.com>
2022-06-15 16:37:47 +0545

    user input handling and generation by bot

--- index.py
+++ index.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from conversation import *
+import json
+
 app = Flask(__name__)
 
 @app.route('/', methods =['GET', 'POST'])
 def ChatbotHome():
     return render_template("index.html")
+
+# Gets user's input, runs it through the talk function in conversation.py and generates the output
+@app.route('/getresponse',methods=['POST'])
+def response():
+    user_inp = request.get_json()
+    text = user_inp['text']
+    prev_conv = user_inp['previous_conversaion']
+    out = talk(text,prev_conv)
+    return jsonify(out)
 
 if __name__ == '__main__':
     app.run(debug=True)
