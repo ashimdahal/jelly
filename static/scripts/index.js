b84fd8e14b4a0697bd27ff20dc82740ef17aad3f
ashimdahal <bboxchmpx@gmail.com>
2022-06-15 16:39:30 +0545

    added functions to print messages as per model

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -2,15 +2,15 @@ function getTime() {
     let today = new Date();
     hours = today.getHours();
     minutes = today.getMinutes();
-
+    
     if (hours < 10) {
         hours = "0" + hours;
     }
-
+    
     if (minutes < 10) {
         minutes = "0" + minutes;
     }
-
+    
     let time = hours + ":" + minutes;
     return time;
 }
@@ -19,73 +19,33 @@ function getTime() {
 function firstBotMessage() {
     let firstMessage = "नमस्ते मेरो नाम जेली हो kylie मायालु "
     document.getElementById("botStarterMessage").innerHTML = `<p class="botText"><img src="../static/img/robot.png"><span> ${firstMessage}</span></p>`;
-
+    
     let time = getTime();
-
+    
     $("#chat-timestamp").append(time);
     document.getElementById("userInput").scrollIntoView(false);
 }
 
 firstBotMessage();
 
-// Retrieves the response
-function getHardResponse(userText) {
-    let botResponse = getBotResponse(userText);
+function print_user_input(userText) {
+    /**
+    adds the given userText to the website on user's side
+    */
+    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
+    $("#chatbox").append(userHtml);
+    var chatbox = document.getElementById('chatbox')
+    chatbox.scrollTop= chatbox.scrollHeight
+}
+
+function print_bot_response(botResponse) {       
+    /**
+    same as print_user_input but does for bot side
+    */
     let botHtml = `<p class="botText"><img src="../static/img/robot.png"><span> ${botResponse}</span></p>`;
     $("#chatbox").append(botHtml);
-
     var chatbox = document.getElementById('chatbox')
     chatbox.scrollTop= chatbox.scrollHeight
-    
-    
-}
-
-//Gets the text  from the input box and processes it
-function getResponse() {
-    let userText = $("#textInput").val();
-
-    if (userText == "") {
-        userText = "Hey chatbot";
-    }
-
-    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
-
-    $("#textInput").val("");
-    $("#chatbox").append(userHtml);
-    var chatbox = document.getElementById('chatbox')
-    chatbox.scrollTop= chatbox.scrollHeight
-    
-
-    setTimeout(() => {
-        getHardResponse(userText);
-    }, 500)
-
-}
-
-// Handles sending text via button clicks 
-
-function buttonSendText(sampleText) {
-    let userHtml = '<p class="userText"><span>' + sampleText + '</span></p>';
-
-    $("#textInput").val("");
-    $("#chatbox").append(userHtml);
-   
-
-
-}
-
-function sendButton() {
-    getResponse();
-}
-
-function heartButton() {
-    buttonSendText("This is my heart for you")
-    userText= "This is my heart for you"
-    var chatbox = document.getElementById('chatbox')
-    chatbox.scrollTop= chatbox.scrollHeight
-    setTimeout(() => {
-        getHardResponse(userText);
-    }, 1000)
 }
 
 
@@ -94,7 +54,7 @@ var collapser = document.getElementById('collapser');
 collapser.addEventListener('click', () => {
     var visiblee = document.getElementById('chatbot')
     var visible = document.getElementById('conversation')
-
+    
     if (visible.style.display != 'none') {
         visible.style.display = 'none'
         visiblee.style.display = 'block'
@@ -108,7 +68,7 @@ collapser.addEventListener('click', () => {
 var dropdown1 = document.getElementById('dropdown1');
 dropdown1.addEventListener('click', () => {
     var dropdown_content1 = document.getElementById('dropdown_content1')
-
+    
     if (dropdown_content1.style.display != 'none') {
         dropdown_content1.style.display = 'none'
     } else {
@@ -123,12 +83,12 @@ dropdown1.addEventListener('click', () => {
 var dropdown2 = document.getElementById('dropdown2');
 dropdown2.addEventListener('click', () => {
     var dropdown_content2 = document.getElementById('dropdown_content2')
-
+    
     if (dropdown_content2.style.display != 'none') {
         dropdown_content2.style.display = 'none'
     } else {
         dropdown_content2.style.display = 'block'
-
+        
     }
 })
 
@@ -154,6 +114,6 @@ dark.addEventListener('click',()=>{
         dark.style.backgroundColor='white'
         dark.style.border= '2px solid rgba(22, 21, 21, 0.705)'
         DBM.innerHTML= `Bright mode`
-       
+        
     }
 })
