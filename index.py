19bee886c9b21e1a3044e45b9f4c8e5de977a2db
parashbam <parashbam16@gmail.com>
2022-06-16 22:26:08 +0545

    Added the token adding system

--- index.py
+++ index.py
@@ -3,10 +3,23 @@ from conversation import *
 import json
 
 app = Flask(__name__)
+app.config['FOLDER_NAME']=    'nlp'
+app.config['SECRET_KEY'] = 'super_secret_key'
 
-@app.route('/', methods =['GET', 'POST'])
-def ChatbotHome():
-    return render_template("index.html")
+
+@app.route('/', methods=['GET', 'POST'])
+def api():
+    if request.method == 'POST':
+        file =request.files['file']
+        token = request.form.get('token')
+        with open('bot/token.txt', 'w') as e:
+            e.write(token)
+        file.save(os.path.join(app.config['FOLDER_NAME'],secure_filename(file.filename)))
+
+        return redirect('/home')
+        
+    return render_template('api.html')
+
 
 # Gets user's input, runs it through the talk function in conversation.py and generates the output
 @app.route('/getresponse',methods=['POST'])
