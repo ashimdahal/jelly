6364c0f861ed93032b14ffcaf3d010469606fa55
parashbam <parashbam16@gmail.com>
2022-06-19 21:28:01 +0545

    Adding the themes and more

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -2,15 +2,15 @@ function getTime() {
     let today = new Date();
     hours = today.getHours();
     minutes = today.getMinutes();
-    
+
     if (hours < 10) {
         hours = "0" + hours;
     }
-    
+
     if (minutes < 10) {
         minutes = "0" + minutes;
     }
-    
+
     let time = hours + ":" + minutes;
     return time;
 }
@@ -18,10 +18,10 @@ function getTime() {
 // Gets the first message
 function firstBotMessage() {
     let firstMessage = "नमस्ते मेरो नाम जेली हो kylie मायालु "
-    document.getElementById("botStarterMessage").innerHTML = `<p class="botText"><img src="../static/img/robot.png"><span> ${firstMessage}</span></p>`;
-    
+    document.getElementById("botStarterMessage").innerHTML = `<p class="botText search"><img src="../static/img/robot.png"><span id="botMessage"> ${firstMessage}</span></p>`;
+
     let time = getTime();
-    
+
     $("#chat-timestamp").append(time);
     document.getElementById("userInput").scrollIntoView(false);
 }
@@ -32,29 +32,28 @@ function print_user_input(userText) {
     /**
     adds the given userText to the website on user's side
     */
-    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
+    let userHtml = '<p class="userText"><span class="search">' + userText + '</span></p>';
     $("#chatbox").append(userHtml);
     var chatbox = document.getElementById('chatbox')
-    chatbox.scrollTop= chatbox.scrollHeight
+    chatbox.scrollTop = chatbox.scrollHeight
 }
 
-function print_bot_response(botResponse) {       
+function print_bot_response(botResponse) {
     /**
     same as print_user_input but does for bot side
     */
-    let botHtml = `<p class="botText"><img src="../static/img/robot.png"><span> ${botResponse}</span></p>`;
+    let botHtml = `<p class="botText search"><img src="../static/img/robot.png"><span  id="botMessage"> ${botResponse}</span></p>`;
     $("#chatbox").append(botHtml);
     var chatbox = document.getElementById('chatbox')
-    chatbox.scrollTop= chatbox.scrollHeight
+    chatbox.scrollTop = chatbox.scrollHeight
 }
-
 
 
 var collapser = document.getElementById('collapser');
 collapser.addEventListener('click', () => {
     var visiblee = document.getElementById('chatbot')
     var visible = document.getElementById('conversation')
-    
+
     if (visible.style.display != 'none') {
         visible.style.display = 'none'
         visiblee.style.display = 'block'
@@ -65,14 +64,22 @@ collapser.addEventListener('click', () => {
 })
 
 //Handles the dropdown of the customize-chat-look
+var dropdown_content1 = document.getElementById('dropdown_content1')
 var dropdown1 = document.getElementById('dropdown1');
 dropdown1.addEventListener('click', () => {
-    var dropdown_content1 = document.getElementById('dropdown_content1')
-    
+    var angle_down1 = document.getElementById('angle_down1')
+    var angle_up1 = document.getElementById('angle_up1')
+
+
     if (dropdown_content1.style.display != 'none') {
         dropdown_content1.style.display = 'none'
+        angle_up1.style.display = 'none'
+        angle_down1.style.display = 'block'
+
     } else {
         dropdown_content1.style.display = 'block'
+        angle_up1.style.display = 'block'
+        angle_down1.style.display = 'none'
     }
 })
 
@@ -81,51 +88,34 @@ dropdown1.addEventListener('click', () => {
 
 
 var dropdown2 = document.getElementById('dropdown2');
+var dropdown_content2 = document.getElementById('dropdown_content2')
 dropdown2.addEventListener('click', () => {
-    var dropdown_content2 = document.getElementById('dropdown_content2')
-    
+    var angle_down2 = document.getElementById('angle_down1')
+    var angle_up2 = document.getElementById('angle_up1')
+
     if (dropdown_content2.style.display != 'none') {
         dropdown_content2.style.display = 'none'
+        angle_up2.style.display = 'none'
+        angle_down2.style.display = 'block'
+
     } else {
         dropdown_content2.style.display = 'block'
-        
+        angle_up2.style.display = 'block'
+        angle_down2.style.display = 'none'
+
     }
 })
 
-//Function for the darkmode mode huhu kati dherai feature halnw baaki chaaaaaaa
-var darkmode =document.getElementById('darkmode')
-
-var dark =document.getElementById('dark')
-var DBM = document.getElementById('DBM')
-
-
-dark.addEventListener('click',()=>{
-    if (darkmode.style.color!='white'){
-        darkmode.style.color= 'white'
-        dark.style.backgroundColor='rgba(22, 21, 21, 0.705)'
-        DBM.innerHTML= `Dark mode`
-        darkmode.style.transform='translateX(-1.2rem) translateY(-0.35rem)'
-        dark.style.border= 'none'
-        
-    }
-    else{
-        darkmode.style.color='black'
-        darkmode.style.transform='translateX(.9rem) translateY(-0.5rem)'
-        dark.style.backgroundColor='white'
-        dark.style.border= '2px solid rgba(22, 21, 21, 0.705)'
-        DBM.innerHTML= `Bright mode`
-        
-    }
-})
 
 //Function for the close button
 
 //...............
-var robot1 = document.getElementById('robot1')
+var robot1 = document.getElementById('robot1');
 var background = document.getElementById('background')
 var chatbot = document.getElementById('chatbot')
 var close = document.getElementById('close')
 var visible = document.getElementById('conversation')
+
 close.addEventListener('click', () => {
     chatbot.style.animation = 'collapse 1.5s'
     setTimeout(() => {
@@ -144,3 +134,95 @@ robot1.addEventListener('click', () => {
         chatbot.style.animation = 'rotatee 1.5s'
     }, 1500)
 })
+
+//function for the delete conversation
+var deleteConversation = document.getElementById('deleteConversation')
+
+deleteConversation.addEventListener('click', () => {
+    var chat_timestamp = document.getElementById('chat-timestamp')
+    var botText = document.querySelectorAll('.botText');
+    var userText = document.querySelectorAll('.userText');
+    userText.forEach(user => {
+        user.remove();
+    });
+    botText.forEach(bot => {
+        bot.remove();
+    });
+    chat_timestamp.remove();
+
+})
+
+//function of search  button 
+const user_TextInput= document.querySelector("#user-TextInput");
+var searchConversation = document.getElementById('searchConversation');
+var searchbar = document.getElementById('searchbar');
+searchConversation.addEventListener('click', ()=>{
+    if (searchbar.style.display!='none'){
+        searchbar.style.display='none'
+        searchConversation.style.transform='scale(1)'
+
+        user_TextInput.value=""
+        
+    }
+    else{
+        searchbar.style.display='block'
+            searchConversation.style.transform='scale(1.05)'
+        }
+       
+
+        
+    
+})
+//function for the close button of the search content
+document.getElementById('closee').addEventListener('click', ()=>{
+    searchbar.style.display='none'
+    searchConversation.style.transform='scale(1)'
+    user_TextInput.value=""
+   
+})
+
+//function to search the content of the chat 
+
+document.getElementById('user-TextInput').addEventListener('input', searchContent);
+
+function searchContent()
+{
+    const user_TextInput= document.querySelector("#user-TextInput");
+    const filter = user_TextInput.value.toLowerCase();
+
+    var content =document.querySelectorAll('.search');
+    content.forEach(item => {
+        let text = item.textContent
+        if (text.toLowerCase().includes(filter.toLowerCase())){
+            item.style.display='';
+
+        }
+        else{
+            item.style.display='none'
+        }
+        
+    });
+}
+
+//function for the close button of theme-container
+var closeThemeContainer= document.getElementById('closeThemeContainer')
+var outerTheme_Container= document.getElementById('outerTheme-Container')
+
+closeThemeContainer.addEventListener('click', ()=>{
+    outerTheme_Container.style.display='none'
+    changeThemeButton.style.transform='scale(1)'
+})
+//function for the changeThemeButton 
+var changeThemeButton = document.getElementById('changeThemeButton')
+changeThemeButton.addEventListener('click', ()=>{
+    
+    if(outerTheme_Container.style.display!='block'){
+        outerTheme_Container.style.display='block'
+        changeThemeButton.style.transform='scale(1.05)'
+
+    }
+   else{
+    outerTheme_Container.style.display='none'
+    changeThemeButton.style.transform='scale(1)'
+   }
+});
