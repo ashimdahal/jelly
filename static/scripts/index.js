686513bf8c0c2b5efa880752217692f9958168dc
parashbam <parashbam16@gmail.com>
2022-06-26 18:32:03 +0545

    Responsive UI

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -17,8 +17,8 @@ function getTime() {
 
 // Gets the first message
 function firstBotMessage() {
-    let firstMessage = "नमस्ते मेरो नाम जेली हो kylie मायालु "
-    document.getElementById("botStarterMessage").innerHTML = `<p class="botText search"><img src="../static/img/robot.png"><span id="botMessage"> ${firstMessage}</span></p>`;
+    let firstMessage = "Namaste ma tapaiko bot jelly "
+    document.getElementById("botStarterMessage").innerHTML = `<p class="botText search"><img src="../static/img/chatbot1.png"><span class="botMessage" > ${firstMessage}</span></p>`;
 
     let time = getTime();
 
@@ -42,25 +42,77 @@ function print_bot_response(botResponse) {
     /**
     same as print_user_input but does for bot side
     */
-    let botHtml = `<p class="botText search"><img src="../static/img/robot.png"><span  id="botMessage"> ${botResponse}</span></p>`;
+    let botHtml = `<p class="botText search"><img src="../static/img/chatbot1.png"><span class="botMessage" > ${botResponse}</span></p>`;
     $("#chatbox").append(botHtml);
     var chatbox = document.getElementById('chatbox')
     chatbox.scrollTop = chatbox.scrollHeight
 }
 
-
+//function for the i button
+var visiblee = document.getElementById('chatbot')
+var visible = document.getElementById('conversation')
 var collapser = document.getElementById('collapser');
 collapser.addEventListener('click', () => {
-    var visiblee = document.getElementById('chatbot')
-    var visible = document.getElementById('conversation')
-
+    var width = window.innerWidth
+    if (width>1100){
+   
     if (visible.style.display != 'none') {
         visible.style.display = 'none'
         visiblee.style.display = 'block'
     } else {
         visible.style.display = 'block'
         visiblee.style.display = 'grid'
+        visiblee.style.gridTemplateColumns='3.5fr 1fr'
+        console.log('ram')
     }
+}
+else if (width<=1100 && width>=790){
+    if (visible.style.display != 'none') {
+        visible.style.display = 'none'
+        visiblee.style.display = 'block'
+        
+    } else {
+        visible.style.display = 'block'
+        visiblee.style.display = 'grid'
+        visiblee.style.gridTemplateColumns='2.5fr 1fr'
+        console.log('santosh')
+        
+    }
+}
+else if (width<=790 && width>=636 ){
+    if (visible.style.display != 'none') {
+        visible.style.display = 'none'
+        visiblee.style.display = 'block'
+        
+    } else {
+        visible.style.display = 'block'
+        visiblee.style.display = 'grid'
+        visiblee.style.gridTemplateColumns='2fr 1fr'
+        console.log('parash')
+        
+        
+    }
+}
+else if (width<=635){
+    var flexer = document.getElementById('flexer')
+    if (visible.style.display != 'none') {
+        visible.style.display = 'none'
+        flexer.style.display = 'block'
+        
+    } else {
+        visible.style.display = 'block'
+        flexer.style.display = 'none'
+        
+        
+    }
+}
+})
+
+//Function for the back button of the conversational info
+var back = document.getElementById('back')
+back.addEventListener('click', () => {
+    visible.style.display = 'none'
+    flexer.style.display = 'block'
 })
 
 //Handles the dropdown of the customize-chat-look
@@ -69,6 +121,7 @@ var dropdown1 = document.getElementById('dropdown1');
 dropdown1.addEventListener('click', () => {
     var angle_down1 = document.getElementById('angle_down1')
     var angle_up1 = document.getElementById('angle_up1')
+
 
 
     if (dropdown_content1.style.display != 'none') {
@@ -86,12 +139,12 @@ dropdown1.addEventListener('click', () => {
 
 //Handles the dropdown of the customise conversation
 
-
 var dropdown2 = document.getElementById('dropdown2');
 var dropdown_content2 = document.getElementById('dropdown_content2')
 dropdown2.addEventListener('click', () => {
-    var angle_down2 = document.getElementById('angle_down1')
-    var angle_up2 = document.getElementById('angle_up1')
+
+    var angle_down2 = document.getElementById('angle_down2')
+    var angle_up2 = document.getElementById('angle_up2')
 
     if (dropdown_content2.style.display != 'none') {
         dropdown_content2.style.display = 'none'
@@ -104,35 +157,9 @@ dropdown2.addEventListener('click', () => {
         angle_down2.style.display = 'none'
 
     }
-})
 
 
-//Function for the close button
 
-//...............
-var robot1 = document.getElementById('robot1');
-var background = document.getElementById('background')
-var chatbot = document.getElementById('chatbot')
-var close = document.getElementById('close')
-var visible = document.getElementById('conversation')
-
-close.addEventListener('click', () => {
-    chatbot.style.animation = 'collapse 1.5s'
-    setTimeout(() => {
-        chatbot.style.display = 'none';
-        background.style.display = 'flex'
-        background.style.animation = 'rotatee 1.5s'
-    }, 1500)
-
-})
-robot1.addEventListener('click', () => {
-    background.style.animation = 'collapse 1.5s'
-    setTimeout(() => {
-        background.style.display = 'none';
-        chatbot.style.display = 'grid'
-        visible.style.display = 'block'
-        chatbot.style.animation = 'rotatee 1.5s'
-    }, 1500)
 })
 
 //function for the delete conversation
@@ -157,18 +184,41 @@ const user_TextInput= document.querySelector("#user-TextInput");
 var searchConversation = document.getElementById('searchConversation');
 var searchbar = document.getElementById('searchbar');
 searchConversation.addEventListener('click', ()=>{
+    var width = window.innerWidth
+    // For bigger screens
+    if (width>=636){
     if (searchbar.style.display!='none'){
         searchbar.style.display='none'
         searchConversation.style.transform='scale(1)'
 
         user_TextInput.value=""
+        var content =document.querySelectorAll('.search');
+        content.forEach(item => {
+                item.style.display='';
+            
+        });
         
     }
     else{
         searchbar.style.display='block'
-            searchConversation.style.transform='scale(1.05)'
+            searchConversation.style.transform='scale(1.04)'
         }
-       
+    }
+    // for smaller screens
+    else{
+        searchbar.style.display='block'
+        user_TextInput.value=""
+        user_TextInput.value=""
+        var content =document.querySelectorAll('.search');
+        content.forEach(item => {
+                item.style.display='';
+            
+        });
+        visible.style.display = 'none'
+        flexer.style.display = 'block'
+            
+        }
+    
 
         
     
@@ -178,6 +228,11 @@ document.getElementById('closee').addEventListener('click', ()=>{
     searchbar.style.display='none'
     searchConversation.style.transform='scale(1)'
     user_TextInput.value=""
+    var content =document.querySelectorAll('.search');
+    content.forEach(item => {
+            item.style.display='';
+        
+    });
    
 })
 
@@ -215,6 +270,9 @@ closeThemeContainer.addEventListener('click', ()=>{
 //function for the changeThemeButton 
 var changeThemeButton = document.getElementById('changeThemeButton')
 changeThemeButton.addEventListener('click', ()=>{
+    var width = window.innerWidth
+    // For bigger screens
+    if (width>=636){
     
     if(outerTheme_Container.style.display!='block'){
         outerTheme_Container.style.display='block'
@@ -224,5 +282,12 @@ changeThemeButton.addEventListener('click', ()=>{
    else{
     outerTheme_Container.style.display='none'
     changeThemeButton.style.transform='scale(1)'
+   }}
+   else{
+    outerTheme_Container.style.display='block'
+    visible.style.display = 'none'
+    flexer.style.display = 'block'
+
    }
 });
+// resoponsive JS
--- static/scripts/theme.js
+++ static/scripts/theme.js
@@ -1,20 +1,23 @@
 //Function for the dark mode
 var darkmode = document.getElementById('darkmode');
 var dark = document.getElementById('dark');
+var main_section = document.getElementById('main-section')
 var dropdown_content1 = document.getElementById('dropdown_content1')
 var dropdown_content2 = document.getElementById('dropdown_content2')
 var DBM = document.getElementById('DBM');
 var themeicon = document.getElementById('themeicon');
 var chat_icon5 = document.getElementById('chat-icon5')
 var textInput = document.getElementById('textInput')
-var botMessage = document.getElementById('botMessage')
 var usertextInput = document.getElementById('user-TextInput')
 var dark = document.getElementById('dark');
 var searchFontIcon = document.getElementById('searchFontIcon')
 var close_theme = document.getElementById('close-Theme')
+var searchbar = document.getElementById('searchbar')
+var chatbox = document.getElementById('chatbox')    
 dark.addEventListener('click', () => {
     if (darkmode.style.color != 'white') {
         darkmode.style.color = 'white'
+        main_section.style.background = '#fafafa'
         chatbot.style.background = '#fafafa'
         chatbot.style.color = '#121212'
         textInput.style.backgroundColor = '#e0e0e0'
@@ -29,17 +32,22 @@ dark.addEventListener('click', () => {
         chat_icon5.style.color = 'rgb(34, 33, 33)'
         DBM.innerHTML = `Dark mode`
         DBM.style.backgroundColor = 'rgba(0, 0, 0, 0.685)'
-        darkmode.style.transform = 'translateX(-1.2rem) translateY(-0.35rem)'
+        darkmode.style.transform = 'translateX(-.8rem) translateY(-0.35rem)'
         dark.style.border = 'none'
-        botMessage.style.color = '#121212'
-        botMessage.style.backgroundColor = '#e0e0e0'
+     
+        close_theme.style.color = '#121212'      
         usertextInput.style.backgroundColor = '#f0f2f5'
         usertextInput.style.color = '#121212'
+        searchbar.style.backgroundColor='#fafafa'
+        chatbox.classList.remove('darkchatbox')
+        chatbox.classList.add('chatbox')
+        
 
     } else {
         darkmode.style.color = 'black'
         darkmode.style.transform = 'translateX(.9rem) translateY(-0.5rem)'
         dark.style.backgroundColor = 'white'
+        main_section.style.background = '#121212'
         chatbot.style.background = '#121212'
         dropdown_content1.classList.add('darktheme')
         dropdown_content1.classList.remove('content')
@@ -47,7 +55,7 @@ dark.addEventListener('click', () => {
         dropdown_content2.classList.remove('content')
         dropdown_content2.classList.remove('darkredtheme')
         dropdown_content1.classList.remove('darkredtheme')
-
+        close_theme.style.color = '#121212'
         chatbot.style.color = '#fafafa'
         close_theme.style.color = '#121212'
         textInput.style.backgroundColor = 'rgb(84 84 84 / 48%)'
@@ -55,18 +63,22 @@ dark.addEventListener('click', () => {
         dark.style.border = '2px solid rgba(22, 21, 21, 0.705)'
         DBM.innerHTML = `Bright mode`
         DBM.style.backgroundColor = 'rgba(0, 0, 0, 0.685)'
+        console.log(DBM)
         chat_icon5.style.color = '#dbdbdb'
-        botMessage.style.color = '#fafafa'
-        botMessage.style.backgroundColor = 'rgb(84 84 84 / 48%)'
         usertextInput.style.backgroundColor = 'rgb(84 84 84 / 48%)'
         usertextInput.style.color = '#fafafa'
         searchFontIcon.style.color = '#121212'
+        searchbar.style.backgroundColor='#121212'
+        chatbox.classList.remove('chatbox')
+        chatbox.classList.add('darkchatbox')
+        
     }
 });
 //Function for dark-red theme
 
 var red_theme = document.getElementById('dark-red-theme')
 red_theme.addEventListener('click', () => {
+    var botMessage = document.getElementById('botMessage')
     chatbot.style.background = 'darkred'
     dropdown_content1.classList.add('darkredtheme')
     dropdown_content1.classList.remove('content')
@@ -85,8 +97,17 @@ red_theme.addEventListener('click', () => {
     chat_icon5.style.color = '#dbdbdb'
     botMessage.style.color = '#fafafa'
     botMessage.style.backgroundColor = 'rgb(190 19 19)'
-    usertextInput.style.backgroundColor = 'rgb(84 84 84 / 48%)'
-    usertextInput.style.color = '#fafafawhite'
-    searchFontIcon.style.color = '#121212'
+    usertextInput.style.backgroundColor = 'gb(190 19 19)'
+    usertextInput.style.color = '#fafafa'
+    searchFontIcon.style.color = '#fafafa'
+    searchbar.style.backgroundColor='darkred'
+    searchbar.style.color='#fafafa'
 
 })
+//customized send button
+chat_icon5.addEventListener('mousedown', ()=>{
+    chat_icon5.style.transform = 'scale(.9)';
+  })
+  chat_icon5.addEventListener('mouseup', ()=>{
+    chat_icon5.style.transform = 'scale(1)';
+  })
